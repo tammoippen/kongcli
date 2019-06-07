b2d42f691fedba5ae0b45debb6c13a2387c744da
Tammo Ippen <tammo.ippen@posteo.de>
2019-06-07 18:02:24 +0200

    better help

--- src/kongcli/_cli.py
+++ src/kongcli/_cli.py
@@ -2,6 +2,7 @@ from functools import partial
 import json
 
 import click
+from tabulate import tabulate_formats
 
 click.option = partial(click.option, show_default=True)  # type: ignore # noqa: E402
 
@@ -15,8 +16,8 @@ from ._services import list_services
 @click.group()
 @click.option("--url", envvar="KONG_BASE", help="Base url to kong.")
 @click.option("--apikey", envvar="KONG_APIKEY", help="API key for key-auth to kong.")
-@click.option("--tablefmt", default="fancy_grid", help="Format for the output tables.")
-@click.option("--font", default="banner", help="Font for the table headers.")
+@click.option("--tablefmt", default="fancy_grid", help=f"Format for the output tables. Supported formats: {', '.join(tabulate_formats)}")
+@click.option("--font", default="banner", help="Font for the table headers. See http://www.figlet.org/examples.html for examples.")
 @click.pass_context
 def cli(ctx: click.Context, url: str, apikey: str, tablefmt: str, font: str) -> None:
     """Interact with your kong admin api."""
