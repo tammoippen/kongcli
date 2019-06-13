f7ca7d10c5852fd100567d4cf2505ab8cdb7b0b7
Tammo Ippen <tammo.ippen@posteo.de>
2019-06-13 14:44:15 +0200

    Some more tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,7 @@
+import os
+
+import psycopg2
+from psycopg2.extras import DictCursor
 import pytest
 
 from kongcli._session import LiveServerSession
@@ -5,4 +9,22 @@ from kongcli._session import LiveServerSession
 
 @pytest.fixture()
 def session():
-    return LiveServerSession('http://localhost:8001')
+    return LiveServerSession("http://localhost:8001")
+
+
+@pytest.fixture()
+def clean_kong():
+    with psycopg2.connect(
+        database=os.environ.get("KONG_PG_DATABASE", "kong"),
+        user=os.environ.get("KONG_PG_USER", "kong"),
+        password=os.environ.get("KONG_PG_PASSWORD", "kong"),
+        host="localhost",
+        cursor_factory=DictCursor,
+    ) as conn, conn.cursor() as cursor:
+        cursor.execute(
+            """SELECT table_name
+                          FROM information_schema.tables
+                          WHERE table_schema = 'public' AND table_name <> 'schema_migrations';"""
+        )
+        tables = [row[0] for row in cursor.fetchall()]
+        cursor.execute(f"TRUNCATE TABLE {', '.join(tables)} CASCADE;")
--- tests/test_kong.py
+++ tests/test_kong.py
@@ -1,7 +1,55 @@
-from kongcli._kong import information
+from math import ceil
+from operator import itemgetter
+import os
+from time import time
+import uuid
+
+import pytest
+
+from kongcli._kong import add, all_of, information
 
 
-def test_information(session):
+def test_information(session, clean_kong):
     resp = information(session)
     assert isinstance(resp, dict)
-    assert '0.13.1' == resp['version']
+    assert os.environ.get("KONG_VERSION_TAG", "0.13.1") == resp["version"]
+
+
+@pytest.mark.parametrize(
+    "resource",
+    ("consumers", "services", "routes", "plugins", "acls", "key-auths", "basic-auths"),
+)
+def test_all_of_empty(resource, session, clean_kong):
+    assert all_of(resource, session) == []
+
+
+def test_add_all_of_consumer(session, clean_kong):
+    consumer1 = add("consumers", session, username="test-user", custom_id="1234")
+    assert consumer1["username"] == "test-user"
+    assert consumer1["custom_id"] == "1234"
+    assert uuid.UUID(consumer1["id"])
+    assert consumer1["created_at"] <= ceil(time()) * 1000
+
+    consumer2 = add("consumers", session, custom_id="12345")
+    assert "username" not in consumer2
+    assert consumer2["custom_id"] == "12345"
+    assert uuid.UUID(consumer2["id"])
+    assert consumer2["created_at"] <= ceil(time()) * 1000
+
+    consumer3 = add("consumers", session, username="test-user2")
+    assert consumer3["username"] == "test-user2"
+    assert "custom_id" not in consumer3
+    assert uuid.UUID(consumer3["id"])
+    assert consumer3["created_at"] <= ceil(time()) * 1000
+
+    consumers = all_of('consumers', session)
+    assert 3 == len(consumers)
+    assert sorted([consumer1, consumer2, consumer3], key=itemgetter('id')) == sorted(consumers, key=itemgetter('id'))
+
+
+def test_add_all_of_consumer_paginate(session, clean_kong):
+    for i in range(201):
+        add("consumers", session, custom_id=str(i))
+
+    consumers = all_of('consumers', session)
+    assert 201 == len(consumers)
