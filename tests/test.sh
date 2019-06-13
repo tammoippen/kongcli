e6ea5b12a960742eb2c9ecc4b96bafa00aa9617b
Tammo Ippen <tammo.ippen@posteo.de>
2019-06-13 11:29:08 +0200

    Got ci running locally

---
+++ tests/test.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+set -x
+
+# meant for running in the docker image / ci
+apk add --no-cache build-base  # typed-ast
+poetry install
+poetry run flake8
+poetry run mypy src
+poetry run pytest
