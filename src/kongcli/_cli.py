f2c2b14e47429a41cf52fccd7b051d506f2d1368
Tammo Ippen <tammo.ippen@posteo.de>
2019-07-03 17:42:36 +0200

    Add a raw command to execute arbitrary commands to kongh

--- src/kongcli/_cli.py
+++ src/kongcli/_cli.py
@@ -1,7 +1,7 @@
 from functools import partial
 import json
 import sys
-from typing import Optional
+from typing import Optional, Tuple
 
 import click
 from loguru import logger
@@ -14,7 +14,7 @@ from ._plugins import list_global_plugins, plugins_cli
 from ._routes import list_routes
 from ._services import list_services
 from ._session import LiveServerSession
-from ._util import get
+from ._util import dict_from_dot, get
 from .kong.general import information
 
 
@@ -53,11 +53,11 @@ def cli(
     ctx.ensure_object(dict)
     logger.remove()
     if verbose == 1:
-        logger.add(sys.stdout, level="WARNING")
+        logger.add(sys.stderr, level="WARNING")
     if verbose == 2:
-        logger.add(sys.stdout, level="INFO")
+        logger.add(sys.stderr, level="INFO")
     if verbose == 3:
-        logger.add(sys.stdout, level="DEBUG")
+        logger.add(sys.stderr, level="DEBUG")
 
     session = LiveServerSession(url)
     if apikey:
@@ -82,6 +82,80 @@ def info(ctx: click.Context) -> None:
     """Show information on the kong instance."""
     info = get("information", lambda: information(ctx.obj["session"]))
     print(json.dumps(info, indent=2))
+
+
+@cli.command()
+@click.option("--header", "-H", type=(str, str), multiple=True, help="Add headers.")
+@click.option(
+    "--data",
+    "-d",
+    type=(str, str),
+    multiple=True,
+    help="Add key-value data points to the payload.",
+)
+@click.argument("method")
+@click.argument("url")
+@click.pass_context
+def raw(
+    ctx: click.Context,
+    method: str,
+    url: str,
+    header: Tuple[Tuple[str, str], ...],
+    data: Tuple[Tuple[str, str], ...],
+) -> None:
+    """Perform raw http requests to kong.
+
+    You can provide headers using the --header / -H option:
+
+    - to get the header 'Accept: application/json' use
+
+        -H Accept application/json
+
+    - to get the header 'Content-Type: application/json; charset=utf-8' use
+
+        -H Content-Type "application/json; charset=utf-8"
+
+    You can provide a json body using the --data / -d option
+
+      -d foo bar          # => {"foo": "bar"}
+
+      -d foo true         # => {"foo": true}
+
+      -d foo '"true"'     # => {"foo": "true"}
+
+      -d foo.bar.baz 2.3  # => {"foo": {"bar": {"baz": 2.3}}}
+
+      -d name bar -d config.methods '["GET", "POST"]'
+
+      # => {"name": "bar", "config": {"methods": ["GET", "POST"]}}
+
+    If first argument to `--data / -d` is the key. It is split by dots
+    and sub-dictionaries are created. The second argument is assumed to be
+    valid JSON; if it cannot be parsed, we assume it is a string. Multiple
+    usages of `--data / -d` will merge the dictionary.
+    """
+    session: LiveServerSession = ctx.obj["session"]
+    headers_dict = {h[0]: h[1] for h in header}
+    print(f'> {method} {session.prefix_url}{url}', file=sys.stderr)
+    for k, v in session.headers.items():
+        print('> ', k, ': ', v, sep='', file=sys.stderr)
+    for k, v in headers_dict.items():
+        print('> ', k, ': ', v, sep='', file=sys.stderr)
+    print('>', file=sys.stderr)
+
+    payload = None
+    if data:
+        payload = dict_from_dot(data)
+    if payload:
+        print('> Body:')
+        print('>', json.dumps(payload))
+
+    resp = session.request(method, url, headers=headers_dict, json=payload)
+    print(f'< http/{resp.raw.version}', resp.status_code, resp.reason, file=sys.stderr)
+    for k, v in resp.headers.items():
+        print('< ', k, ': ', v, sep='', file=sys.stderr)
+    print(file=sys.stderr)
+    print(resp.text)
 
 
 @cli.group(name="list", chain=True)
--- src/kongcli/_util.py
+++ src/kongcli/_util.py
@@ -1,7 +1,8 @@
-from typing import Any, Callable
+import json
+from typing import Any, Callable, Dict, Tuple
 
 from cachetools import LRUCache
-
+from loguru import logger
 
 CACHE = None
 
@@ -13,3 +14,26 @@ def get(key: str, fkt: Callable[[], Any]) -> Any:
     if key not in CACHE:
         CACHE[key] = fkt()
     return CACHE[key]
+
+
+def dict_from_dot(data: Tuple[Tuple[str, str], ...]) -> Dict[str, Any]:
+    result = {}
+    for k, v in data:
+        key_parts = k.split(".")
+        curr = result
+        for key in key_parts[:-1]:
+            if key not in result:
+                curr[key] = {}
+            curr = curr[key]
+            assert isinstance(
+                curr, dict
+            ), f"The key `{key}` is previously assigned to something else than a dict."
+        value = v
+        try:
+            value = json.loads(v)
+        except json.JSONDecodeError:
+            logger.info(f"Cannot parse `{v}` to json, assuming string.")
+            pass
+        curr[key_parts[-1]] = value
+
+    return result
