c72e6fe4228ee217f32134c5d6a4d141a5497a56
Tammo Ippen <tammo.ippen@posteo.de>
2019-09-23 15:27:53 +0200

    also check . black

--- tests/test.sh
+++ tests/test.sh
@@ -4,6 +4,7 @@ set -x
 # meant for running in the docker image / ci
 apk add --no-cache build-base  # typed-ast
 poetry install
+poetry run black --check .
 poetry run flake8
 poetry run mypy src
 poetry run pytest
